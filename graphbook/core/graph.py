62e7575304ef0b01eef21c4c5a32372f89f6ae72
dailing <qzyz_dailing@163.com>
2020-09-08 02:09:45 +0000

    adding new branch

--- graphbook/core/graph.py
+++ graphbook/core/graph.py
@@ -8,10 +8,15 @@ from .registry import Registry, build_from_cfg
 from torch.nn import Module
 import types
 from ..util import get_logger
+from queue import Queue
+import importlib
 
 logger = get_logger('graph')
 REGISTRY = Registry('root')
 
+
+class NotInitedException(Exception):
+    pass
 
 class _InPort():
     name=None
@@ -23,153 +28,125 @@ class _InPort():
         self.from_node = from_node
         self.from_node_key = from_node_key
 
-class Node(Module):
-
-    def __init__(self, func, default_args=None):
+"""
+    expression format  @name_in_dict  :  __get__ list  :  result_index  :  __get__ list
+"""
+class Session(Module):
+    def __init__(self):
         super().__init__()
+        self._locals = dict()
+        self._result = dict()
 
-        self.default_args = default_args
-        self.func = func
-
-        self.argmap = {}
-        self.current_id = None
-        self.result = None
-        # self.sig = signature(func)
-        assert func is not None
-        assert self.func is not None
-
-
-    def _eval(self, _id=None):
-        # logger.info(f'eval {self.func}')
-        # logger.info(self.argmap)
-        argmap = {}
-        # print(self.argmap)
-        if _id == self.current_id:
-            return self.result
-        self.current_id = _id
-        if self.default_args is not None:
-            for k,v in self.default_args.items():
-                argmap[k] = v
-        for k, v in self.argmap.items():
-            tmp_result = v.from_node._eval(_id)
-            if v.from_node_key == '' or v.from_node_key is None or v.from_node_key=='*':
-                pass
-            elif hasattr(tmp_result, '__getitem__'):
-                tmp_result = tmp_result[v.from_node_key]
-            elif hasattr(tmp_result, v.from_node_key):
-                tmp_result = getattr(tmp_result, v.from_node_key)
-            argmap[k] = tmp_result
-        # parpare arguments
-        nargs = -1
-        for k in  argmap.keys():
-            if isinstance(k, int):
-                nargs = max(k, nargs)
-        args=[]
-        for i in range(nargs+1):
-            if not i in argmap:
-                raise AttributeError(f"FUCK no {i} in the fucking argmap")
-            args.append(argmap[i])
-        kwargs = {}
-        # logger.info(argmap)
-        for k, v in argmap.items():
-            if not isinstance(k, int):
-                if k == '*':
-                    if not hasattr(v, '__getitem__'):
-                        v = [v]
-                    args = v
-                elif k == '' or k is None:
-                    # logger.info("here")
-                    args = [v]
-                elif k == '**':
-                    kwargs.update(v)
-                else:
-                    kwargs[k] = v
-        try:
-            self.result = self.func(*args, **kwargs)
-        except Exception as e:
-            logger.error(self.func)
-            logger.error(f'FUCKING ERROR, args are: ## {args}, ###{kwargs}', exc_info=True)
-            raise e
-        return self.result
-
-    def __call__(self, name=None):
-        result =  self._eval(_id=random.random())
-        if name is None:
-            return result
-        rr = {}
-        if isinstance(name, str):
-            return result[name]
-        for n in name:
-            rr[n] = result[n]
-        return rr
-
-
-@REGISTRY.register_module()
-def Const(value):
-    return lambda : value
-
-@REGISTRY.register_module()
-def Math(func):
-    return getattr(math, func)
-
-def connect(n1, k1, n2, k2):
-    # connect key1 form n1 output to key2 from n2 input
-    port = _InPort(None, n1, k1)
-    n2.argmap[k2] = port
-
-def build_graph(cfg):
-    nodes = {}
-    for k in list(cfg.keys()):
-        if not isinstance(cfg[k], dict):
-            cfg.pop(k)
-
-    for k, v in cfg.items():
-        if not isinstance(v, dict):
-            continue
-        if 'type' not in v:
-            logger.warn(f'config {k} not build, no type key')
-            continue
-        if v['type'] == '_wire_':
+    def _eval(self, expression):
+        if not isinstance(expression, str):
+            return expression
+        if not expression.startswith('@'):
+            return expression
+        local_name, g_list, result_name, r_list = expression.split(':')
+        if local_name in self._locals:
+            inst = self._locals[local_name]
+        else:
+            inst = globals()[local_name]
+        inst = Session._get_list(inst, g_list)
+        if result_name == '':
+            return inst
+        if local_name in self._result:
+            result = self._result[local_name]
+        else:
+            node = self._locals[local_name]
+            if node.instance is None:
+                raise NotInitedException
+            assert isinstance(node, Node)
+            args_, kwargs_ = node.args
+            args = []
+            kwargs = {}
+            for k in args_:
+                args.append(self._eval(k))
+            for k, v in kwargs_.items():
+                kwargs[k] = self._eval(v)
+            result = node.instance(*args, **kwargs)
+            self._result[local_name] = result
+        if result_name == '' or result_name == '*':
             pass
         else:
-            default_args = None
-            if 'default_args' in v:
-                default_args = v.pop('default_args')
-            _no_node=False
-            if '_no_node' in v:
-                _no_node = v.pop('_no_node')
-            if _no_node:
-                nodes[k] = build_from_cfg(v, REGISTRY)
-            else:
-                nodes[k] = Node(build_from_cfg(v, REGISTRY), default_args)
-    for k, v in cfg.items():
-        if 'type' not in v:
-            continue
-        if v['type'] == '_wire_':
-            # logger.info(f'connecting {v}')
-            if 'from_node_port' not in v:
-                v['from_node_port'] = None
-            if 'to_node_port' not in v:
-                v['to_node_port'] = None
-            connect(
-                nodes[v['from_node']],
-                v['from_node_port'],
-                nodes[v['to_node']],
-                v['to_node_port'])
-    return nodes
-
-
-class Graph(Module):
-    def __init__(self, cfg):
-        super().__init__()
-        nodes = build_graph(cfg)
-        for k, v in nodes.items():
-            self.add_module(k, v)
-
-    def forward(self, names):
-        if isinstance(names, str):
-            return getattr(self, names)()
-        result = {}
-        for name in names:
-            result[name] = getattr(self, name)()
+            result = result[result_name]
+        result = Session._get_list(result, r_list)
         return result
+
+    def _init_instance(self, expression)
+    
+    @staticmethod
+    def _get_list(inst, expression):
+        if expression == '*' or expression == '':
+            return inst
+        for e in expression.split('.'):
+            inst = inst.__get__(e)
+        return inst
+
+    def _init_nodes_(self):
+        req = list(self._locals.keys())
+        self._locals.update(dict(
+            CodeBlock=Node(instance=CodeBlock)
+        ))
+        while len(req) > 0:
+            p_req = []
+            for k in req:
+                node = self._locals[k]
+                if node.instance is not None:
+                    pass
+                if isinstance(node.cls, str):
+                    # handle import
+                    if node.cls.startswith('!'):
+                        node.instance = importlib.import_module(node.cls)
+                    else:
+                        try:
+                            node.instance = self._eval(node.cls)
+                        except NotInitedException:
+                            p_req.append(k)
+            if len(p_req) == len(req):
+                print("Circular dependency!")
+                raise Exception("Fuck circular dependency")
+
+    def from_dict(self, records):
+        for k, v in records.items():
+            self._locals[k] = Node(**v)
+
+
+class CodeBlock():
+    def __init__(self, code):
+        if isinstance(code, str):
+            code = code.split('\n')
+        self._code = code
+        self.globals = None
+
+    def __call__(self, **kwargs):
+        local = kwargs
+        for c in self._code[:-1]:
+            eval(c, self.globals, local)
+        try:
+            result = eval(self._code[-1], self.globals, local)
+            return result
+        except SyntaxError:
+            exec(self._code[-1], self.globals, local)
+        return None
+
+
+class Node(Module):
+
+    def __init__(self, cls, init_args=None, args=None, instance=None):
+        self.cls = cls
+        self.init_args = init_args
+        self.args = args
+        self.instance = instance
+
+    def __getattr__(self, attr):
+        return self.instance.__get__(attr)
+
+
+if __name__ == "__main__":
+    session = Session()
+    session.from_dict(
+        dict(
+            # a=dict(cls=)
+        )
+    )
